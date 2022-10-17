42ed8d6be4efe8dcfd631c9397328798e78dacae
Aladser <aladser@gmail.com>
2022-10-17 11:16:33 +0900

    1

---
+++ CryproPro_Keys/MainForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace CryproPro_Keys
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cpv_Label = new System.Windows.Forms.Label();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cpv_Label
+            // 
+            this.cpv_Label.Location = new System.Drawing.Point(12, 9);
+            this.cpv_Label.Name = "cpv_Label";
+            this.cpv_Label.Size = new System.Drawing.Size(66, 23);
+            this.cpv_Label.TabIndex = 0;
+            this.cpv_Label.Text = "Крипто Про";
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.Location = new System.Drawing.Point(12, 39);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(33, 23);
+            this.keyLabel.TabIndex = 1;
+            this.keyLabel.Text = "Ключ";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(84, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 23);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(84, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 23);
+            this.label2.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 81);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.cpv_Label);
+            this.Name = "MainForm";
+            this.Text = "КриптоПро Ключ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cpv_Label;
+        private System.Windows.Forms.Label keyLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
