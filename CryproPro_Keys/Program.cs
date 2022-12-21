a8aa0792380df52cfbf1b1eaff6bbe57b5c7ed9f
Aladser <aladser@gmail.com>
2022-12-21 16:00:01 +0900

    запись ключа в файл

--- CryproPro_Keys/Program.cs
+++ CryproPro_Keys/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace CryproPro_Keys
@@ -8,6 +9,17 @@ namespace CryproPro_Keys
     {
         public static string CRYPTO_VERSION;
         public static string KEY;
+        static readonly string file = $"{Path.GetDirectoryName(Application.ExecutablePath)}\\CryproPro_key_info.txt";
+
+        // ветка реестра КриптоПро
+        static readonly string subkey1 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData\\S-1-5-18\\Products\\";
+        static readonly string[] subkeyVersions = { "05480A45343B0B0429E4860F13549069",
+                "68A52D936E5ACF24C9F8FE4A1C830BC8",
+                "7AB5E7046046FB044ACD63458B5F481C",
+                "08F19F05793DC7340B8C2621D83E5BE5"
+            };
+        static readonly string subkey2 = "\\InstallProperties";
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
@@ -16,28 +28,29 @@ namespace CryproPro_Keys
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            // Поиск КриптоПро
-            string subKey1 = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData\\S-1-5-18\\Products\\";
-            string[] versions = { "05480A45343B0B0429E4860F13549069",
-                "68A52D936E5ACF24C9F8FE4A1C830BC8",
-                "7AB5E7046046FB044ACD63458B5F481C",
-                "08F19F05793DC7340B8C2621D83E5BE5"
-            };
-            string subKey2 = "\\InstallProperties";
+         
             // Определение разрядности
             RegistryView regView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
             RegistryKey localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, regView);
             int index = 0;
+
             // Поиск ключа
             while (true)
             {
                 try
                 {
-                    if (localKey.OpenSubKey(subKey1 + versions[index] + subKey2) != null)
+                    if (localKey.OpenSubKey(subkey1 + subkeyVersions[index] + subkey2) != null)
                     {
-                        CRYPTO_VERSION = "КриптоПро " + localKey.OpenSubKey(subKey1 + versions[index] + subKey2).GetValue("DisplayVersion").ToString();
-                        KEY = localKey.OpenSubKey(subKey1 + versions[index] + subKey2).GetValue("ProductID").ToString();
+                        RegistryKey registryBranch = localKey.OpenSubKey(subkey1 + subkeyVersions[index] + subkey2);
+                        CRYPTO_VERSION = $"КриптоПро {registryBranch.GetValue("DisplayVersion")}";
+                        KEY = registryBranch.GetValue("ProductID").ToString();
+
+                        // запись ключа в файл
+                        StreamWriter writer = new StreamWriter(file, false);
+                        string text = $"{CRYPTO_VERSION}\n{KEY}";
+                        writer.WriteLine(text);
+                        writer.Close();
+
                         break;
                     }
                     else
